c84c4785f44410007d25e430b3678f35d5c87626
Richard Vankoningsveld <richyvk@gmail.com>
2016-09-30 17:31:05 +1000

    Rewrite to accomodate changes to APH bills page

--- aph-rss.py
+++ aph-rss.py
@@ -7,14 +7,20 @@ import config
 
 # change directory to APH-rss root dir, required for
 # Pythonanywhere scheduled task only
-os.chdir(config.run_path)
+# os.chdir(config.run_path)
 
 # Create variable to trigger RSS update
 new_bills_added = False
 
 # convert txt file storing existing bills to a list
-with open('bills_output.txt', 'a+') as f:
-    existing_bills = f.read().splitlines()
+# create new file if not already present
+try:
+    with open('bills_output.txt', 'r') as f:
+        existing_bills = f.read().splitlines()
+except IOError:
+    open('bills_output.txt', 'w').close()
+    with open('bills_output.txt', 'r') as f:
+        existing_bills = f.read().splitlines()
 
 # This section scrapes the latest updates to bills section of the APH homepage,
 # http://www.aph.gov.au/, extracting the update date, bill name and bill
@@ -22,40 +28,36 @@ with open('bills_output.txt', 'a+') as f:
 # to an RSS feed.
 
 # Open and read the APH homepage
-source = urllib2.urlopen("http://www.aph.gov.au/").read()
+source_url = "http://www.aph.gov.au/Parliamentary_Business/Bills_Legislation"
+source = urllib2.urlopen(source_url).read()
 
-# Create BeutifulSoup object of APH site
+# Create BeautifulSoup object of APH site
 soup = BeautifulSoup(source, "html.parser")
 
 # Locate Bills section of APH page
-bills_div = soup.find(id="tabBills")
-bills_table_rows = bills_div.find_all('tr')
+bills_table = soup.find("table", {"class": "documents"})
+bills_table_rows = bills_table.find_all("tr")
 del bills_table_rows[0]
 bills_table_rows.reverse()
 
 # Create string for each Bill containing date, Bill name, and Bill URL
 for row in bills_table_rows:
-    bill_date = row.find("td", {"class": "date"}).string
-    bill_name = row.find("a").string
-    # bill_anchor = item.find("a")
-    # bill_link = "http://www.aph.gov.au{0}".format(bill_anchor['href'])
+    bill_date = row.find("td", {"class": "date"}).string.encode("utf-8").lstrip()
+    bill_name = row.find("a").string.encode("utf-8")
+    bill_anchor = row.find("a")
+    bill_url = "http://www.aph.gov.au{0}".format(bill_anchor["href"].encode("utf-8"))
 
-    bill_anchor = str(row.find('a')['href'])
-    bill_id = re.search('bId=([rs]{1}[0-9]+)', bill_anchor).group(1)
-    bill_link = ('http://parlinfo.aph.gov.au/parlInfo/search/display/' +
-                 'display.w3p;query=Id%3A%22legislation%2Fbillhome%2F' +
-                 bill_id +
-                 '%22')
-
-    bill_string = "{0}: {1}, {2}".format(bill_date.encode('utf-8'),
-                                         bill_name.encode('utf-8'),
-                                         bill_link.encode('utf-8'))
+    bill_string = "{0}: {1}, {2}".format(bill_date,
+                                         bill_name,
+                                         bill_url)
 
     # Add Bill string to existing bills list if not already present
     if bill_string not in existing_bills:
-        print "Adding new bill: {0}".format(bill_name.encode('utf-8'))
+        print "Adding new bill: {0}".format(bill_name)
         existing_bills.append(bill_string)
         new_bills_added = True
+    else:
+        print "{0} already added today".format(bill_name)
 
 with open('bills_output.txt', 'w') as f:
     for item in existing_bills:
